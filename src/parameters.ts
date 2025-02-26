3d9edee2e2c522d06486a00993ed8b1951bdb637
zhong <zhong.chen@konghq.com>
2025-02-25 19:35:27 -0800

    fix: add list services

--- src/parameters.ts
+++ src/parameters.ts
@@ -1,7 +1,28 @@
 import { z } from "zod";
 
 export const listControlPlaneParameters = z.object({
-  region: z.enum(["us", "eu", "au", "in", "me"]),
-  pageSize: z.number().default(10),
-  pageNumber: z.number().default(1),
-})
+  region: z
+    .enum(["us", "eu", "au", "in", "me"])
+    .optional()
+    .default("us")
+    .describe("konnect region"),
+  pageSize: z.number().optional().default(10),
+  pageNumber: z.number().optional().default(1),
+});
+
+export const listServicesParameters = z.object({
+  controlPlaneId: z.string().describe("The ID of the control plane"),
+  region: z
+    .enum(["us", "eu", "au", "in", "me"])
+    .optional()
+    .default("us")
+    .describe("konnect region"),
+  pageSize: z.number().optional().default(10),
+  pageNumber: z.number().optional().default(1),
+  tags: z
+    .string()
+    .optional()
+    .describe(
+      "A list of tags to filter services. Use comma (,) for AND, forward slash (/) for OR"
+    ),
+});
